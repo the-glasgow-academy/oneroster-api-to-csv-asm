6210ba6c884f9ba2892bc0435c7662f7229f5133
Whiteford <B.Whiteford@tga.org.uk>
2020-08-27 15:17:04 +0100

    Add env file template

---
+++ env.template.ps1
@@ -0,0 +1,3 @@
+$env:OR_URL = read-host -prompt "oneroster url (https://oneroster.example.com/ims/oneroster/v1p1)"
+$env:OR_CI = read-host -prompt "oneroster API client id"
+$env:OR_CS = read-host -prompt "oneroster API client secret"
